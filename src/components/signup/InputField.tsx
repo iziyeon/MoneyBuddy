3b9b092ddff8a7c64354c0433fd52c756c704726
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-03 22:35:06 +0900

    ✨ [new features] 회원가입 input 컴포넌트 생성

---
+++ src/components/signup/InputField.tsx
@@ -0,0 +1,34 @@
+import type { InputHTMLAttributes, JSX } from 'react';
+
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  errorMessage?: string;
+}
+
+export default function InputField({
+  label,
+  id,
+  className = '',
+  errorMessage,
+  type = 'text',
+  ...props
+}: InputFieldProps): JSX.Element {
+  return (
+    <div className="flex flex-col pb-6">
+      {label && (
+        <label htmlFor={id} className="text-xs pb-2 text-font1">
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        type={type}
+        className={`p-3.5 rounded text-sm border border-stroke ${className}`}
+        {...props}
+      />
+      {errorMessage && (
+        <p className="mt-1 text-xs text-error">{errorMessage}</p>
+      )}
+    </div>
+  );
+}
