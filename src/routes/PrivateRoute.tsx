e240daa38498ce8804ec293bcb40ef809e644b49
iziyeon <dlfvkfclfrn@gmail.com>
2025-06-04 19:51:23 +0900

    ♻️ [refactor] 라우터 구조 통일 및 PrivateRoute 안정화 - Router 레이아웃 통일 적용 - PrivateRoute 보호 라우팅 안정화 - 전체 라우팅 구조 정비

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,3 +1,6 @@
+//로그인 안 된 경우 → 강제 로그인 페이지로 이동시킴
+//로그인 된 경우 → 자식 컴포넌트 통과
+
 import { Navigate } from 'react-router-dom';
 import { useAuthStore } from '../stores/useAuthStore';
 import type { ReactNode } from 'react';
@@ -8,10 +11,8 @@ type PrivateRouteProps = {
 
 export default function PrivateRoute({ children }: PrivateRouteProps) {
   const user = useAuthStore(state => state.user);
-
   if (!user) {
     return <Navigate to="/login" replace />;
   }
-
   return <>{children}</>;
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,19 +1,67 @@
 import { Routes, Route } from 'react-router-dom';
+import Homepage from '../pages/Homepage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
-import HomePage from '../pages/Homepage';
+import FindIdPage from '../pages/FindIdPage';
+import MyPage from '../pages/MyPage';
+import SettingsPage from '../pages/SettingsPage';
 import PrivateRoute from './PrivateRoute';
+import DefaultLayout from '../components/layout/DefaultLayout';
+import PublicLayout from '../components/layout/PublicLayout';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
       <Route
         path="/"
         element={
+          <DefaultLayout>
+            <Homepage />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicLayout>
+            <LoginPage />
+          </PublicLayout>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <PublicLayout>
+            <SignupPage />
+          </PublicLayout>
+        }
+      />
+      <Route
+        path="/find-id"
+        element={
+          <PublicLayout>
+            <FindIdPage />
+          </PublicLayout>
+        }
+      />
+
+      <Route
+        path="/mypage"
+        element={
           <PrivateRoute>
-            <HomePage />
+            <DefaultLayout>
+              <MyPage />
+            </DefaultLayout>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <PrivateRoute>
+            <DefaultLayout>
+              <SettingsPage />
+            </DefaultLayout>
           </PrivateRoute>
         }
       />
