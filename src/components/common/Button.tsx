3b630560c12ea6f53acce3b88786bea6836a0fad
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-05 01:47:32 +0900

    ✨ [new features] Button 컴포넌트에 variant 속성 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,40 +1,28 @@
 import type { ButtonHTMLAttributes, JSX } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary';
+  variant?: 'primary' | 'disabled' | 'white' | 'border' | 'grayBtn';
 }
 
 export default function Button({
   children,
+  variant = 'primary',
   ...props
 }: ButtonProps): JSX.Element {
+  const styleSet = {
+    primary: 'bg-primary text-white',
+    disabled: 'bg-disabled text-white',
+    white: 'bg-white text-primary',
+    border: 'bg-white border border-primary text-primary',
+    grayBtn: 'bg-white border border-stroke text-font2',
+  };
+
   return (
-    <button className="text-white text-base px-5 py-3 w-80 rounded">
+    <button
+      {...props}
+      className={`${styleSet[variant]} text-base px-5 py-3 w-80 rounded`}
+    >
       {children}
     </button>
   );
 }
-
-// import React from 'react';
-
-// interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-//   variant?: 'primary' | 'secondary';
-// }
-
-// export default function Button({
-//   children,
-//   variant = 'primary',
-//   ...props
-// }: ButtonProps) {
-//   const baseStyle = 'px-4 py-2 rounded text-white';
-//   const styleMap = {
-//     primary: 'bg-blue-500 hover:bg-blue-600',
-//     secondary: 'bg-gray-500 hover:bg-gray-600',
-//   };
-
-//   return (
-//     <button className={`${baseStyle} ${styleMap[variant]}`} {...props}>
-//       {children}
-//     </button>
-//   );
-// }
