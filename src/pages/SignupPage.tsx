9c01fd0cf603d21abe4ea4e550b36cca046a9b50
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-05 02:06:02 +0900

    ✨ [new features] SignupPage 상태관리 추가

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,32 +1,81 @@
-import type { JSX } from 'react';
+import { useState, type JSX } from 'react';
 import InputField from '../components/signup/InputField';
 import SignupHeader from '../components/signup/SignupHeader';
+import EmailDropdown from '../components/signup/EmailDropdown';
+import Button from '../components/common/Button';
 
 export default function SignupPage(): JSX.Element {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [emailId, setEmailId] = useState('');
+  const [domain, setDomain] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+
+  const isNameEmpty = name.length >= 1;
+  const isNumberEmpty = phone.length >= 1;
+  const isEmailEmpty = emailId.length > 0 && domain.length > 0;
+
   return (
-    <div className="p-10 m-10 border border-slate-950">
-      <SignupHeader
-        title="이름을 알려주세요"
-        description="정확한 이름을 입력해주세요"
-      />
-      {/* <div className="text-xl pb-2">이름을 알려주세요</div>
-      <div className="text-xs pb-6">정확한 이름을 입력해주세요</div> */}
-      <InputField id="name" label="이름" placeholder="예) 홍길동" />
-      {/* <div className="flex flex-col">
-        <InputField label="이름" placeholder="예) 홍길동" />
-        <label htmlFor="name" className="text-xs pb-2">
-          이름
-        </label>
-        <input
-          id="name"
-          className="p-3.5 rounded text-sm border  border-stroke"
-          placeholder="예) 홍길동"
+    <div className=" border border-slate-950">
+      <SignupHeader title="이름을" description="정확한 이름을 입력해주세요" />
+      {isEmailEmpty && (
+        <>
+          <InputField
+            id="password"
+            type="password"
+            label="비밀번호"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="특수문자, 영문, 숫자 포함 10자 이상"
+          />
+          <InputField
+            id="passwordcheck"
+            type="password"
+            label="비밀번호 확인"
+            value={passwordCheck}
+            onChange={e => setPasswordCheck(e.target.value)}
+            placeholder="특수문자, 영문, 숫자 포함 10자 이상"
+          />
+        </>
+      )}
+      {isNumberEmpty && (
+        <div className="flex items-center">
+          <InputField
+            id="email"
+            type="email"
+            label="이메일"
+            value={emailId}
+            placeholder="이메일을 입력해주세요"
+            onChange={e => setEmailId(e.target.value)}
+          />
+          <EmailDropdown selected={domain} onSelect={setDomain} />
+        </div>
+      )}
+
+      {isNameEmpty && (
+        <InputField
+          id="phone"
+          label="휴대폰 번호"
+          placeholder="예) 휴대폰번호를 입력해주세요"
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
         />
-      </div> */}
-      <div className="border border-stroke p-5">
-        <button className="bg-disabled text-white text-base px-5 py-3 w-80 rounded">
+      )}
+
+      <InputField
+        id="name"
+        label="이름"
+        placeholder="예) 홍길동"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+
+      <div className="border border-stroke p-5 mt-4">
+        {/* <button className="bg-red text-white text-base px-5 py-3 w-80 rounded">
           입력완료
-        </button>
+        </button> */}
+        <Button>입력완료</Button>
       </div>
     </div>
   );
