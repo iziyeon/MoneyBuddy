a831c283b8f71d8b82d432265a7d0da65980b531
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-12 23:03:48 +0900

    ✨ [new features] 전문가 페이지 경로 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -11,6 +11,7 @@ import ResetPasswordPage from '../pages/FindPage/ResetPasswordPage';
 import PrivateRoute from './PrivateRoute';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import PublicLayout from '../components/layout/PublicLayout';
+import ExpertPage from '../pages/ExpertPage';
 
 export default function Router() {
   return (
@@ -63,6 +64,7 @@ export default function Router() {
           </PublicLayout>
         }
       />
+      <Route path="/expert" element={<ExpertPage />} />
       <Route
         path="/mypage"
         element={
