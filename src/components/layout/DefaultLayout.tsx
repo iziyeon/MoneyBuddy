d3d13eb3e6c8d8fb02c22bfecf5a0ad1e5c542c8
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-12 23:54:03 +0900

    ♻️ [refactor] BottomNavigation import 경로 수정

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,5 +1,5 @@
 //전체 앱 메인 컨텐츠 영역 틀 담당(헤더는 포함하지 않음)
-import BottomNavigation from '../common/BottomNavigation';
+import BottomNavigation from './BottomNavigation';
 import PageWrapper from './PageWrapper';
 import type { ReactNode } from 'react';
 
