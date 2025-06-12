2098871f1196d50c31a2d53b78c948bc3ee73db5
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-12 23:50:12 +0900

    ♻️ [refactor] ExpertPage 레이아웃 구조 개선 및 PageHeader 컴포넌트 적용

--- src/pages/ExpertPage.tsx
+++ src/pages/ExpertPage.tsx
@@ -1,15 +1,23 @@
-import BottomNavigation from '../components/common/BottomNavigation';
+import { useNavigate } from 'react-router-dom';
+import BottomNavigation from '../components/layout/BottomNavigation';
+import PageHeader from '../components/layout/PageHeader';
+import PageWrapper from '../components/layout/PageWrapper';
 import CategoryNav from '../components/pages/Expert/CategoryNav';
-import ExpertHeader from '../components/pages/Expert/ExpertHeader';
 import MonthlyExpertList from '../components/pages/Expert/MonthlyExpertList';
 
 export default function ExpertPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <ExpertHeader />
+    <PageWrapper>
+      <PageHeader
+        title="머니버디 엑스퍼트"
+        showBackButton={false}
+        rightButtonLabel="상담내역"
+        onRightLabelClick={() => navigate('/')}
+      />
       <CategoryNav />
       <MonthlyExpertList />
       <BottomNavigation />
-    </div>
+    </PageWrapper>
   );
 }
