cfee930ae06d1f0e5ee906d31a75258e2d09e9ce
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-12 22:34:53 +0900

    ✨ [new features] bottom navigation 컴포넌트 추가

---
+++ src/components/common/BottomNavigation.tsx
@@ -0,0 +1,71 @@
+import { NavLink } from 'react-router-dom';
+import homeIcon from '../../assets/icons/common/homeIcon.png';
+import homeIconActive from '../../assets/icons/common/homeIcon_active.png';
+import expertIcon from '../../assets/icons/common/expertIcon.png';
+import expertIconActive from '../../assets/icons/common/expertIcon_active.png';
+import megazineIcon from '../../assets/icons/common/megazineIcon.png';
+import megazineIconActive from '../../assets/icons/common/megazineIcon_active.png';
+import communityIcon from '../../assets/icons/common/communityIcon.png';
+import communityIconActive from '../../assets/icons/common/communityIcon_active.png';
+import userIcon from '../../assets/icons/common/userIcon.png';
+import userIconActive from '../../assets/icons/common/userIcon_active.png';
+
+export default function BottomNavigation() {
+  const navItems = [
+    {
+      id: 'home',
+      label: '홈',
+      path: '/',
+      icon: homeIcon,
+      activeIcon: homeIconActive,
+    },
+    {
+      id: 'expert',
+      label: '전문가',
+      path: '/expert',
+      icon: expertIcon,
+      activeIcon: expertIconActive,
+    },
+    {
+      id: 'megazine',
+      label: '매거진',
+      path: '/megazine',
+      icon: megazineIcon,
+      activeIcon: megazineIconActive,
+    },
+    {
+      id: 'community',
+      label: '커뮤니티',
+      path: '/community',
+      icon: communityIcon,
+      activeIcon: communityIconActive,
+    },
+    {
+      id: 'mypage',
+      label: '마이페이지',
+      path: '/mypage',
+      icon: userIcon,
+      activeIcon: userIconActive,
+    },
+  ];
+
+  return (
+    <div className="fixed bottom-0 flex gap-4 border rounded-t-xl justify-between">
+      {navItems.map(item => (
+        <NavLink
+          key={item.id}
+          to={item.path}
+          className="flex items-center justify-center p-5"
+        >
+          {({ isActive }) => (
+            <img
+              className="w-8"
+              src={isActive ? item.activeIcon : item.icon}
+              alt={item.label}
+            />
+          )}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
