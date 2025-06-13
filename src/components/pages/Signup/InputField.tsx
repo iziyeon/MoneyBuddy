6c7d8384d723558f1ffd02a589207f74d9359065
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-14 02:48:30 +0900

    ♻️ [refactor] 비밀번호 확인 아이콘 추가

--- src/components/pages/Signup/InputField.tsx
+++ src/components/pages/Signup/InputField.tsx
@@ -1,8 +1,10 @@
-import type { InputHTMLAttributes, JSX } from 'react';
+import { useState, type InputHTMLAttributes, type JSX } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   errorMessage?: string;
+  hasToggle?: boolean;
 }
 
 export default function InputField({
@@ -11,8 +13,13 @@ export default function InputField({
   className = '',
   errorMessage,
   type = 'text',
+  hasToggle = false,
   ...props
 }: InputFieldProps): JSX.Element {
+  const [show, setShow] = useState(false);
+  const isPassword = type === 'password';
+  const inputType = isPassword && hasToggle && show ? 'text' : type;
+
   return (
     <div className="flex flex-col pb-6">
       {label && (
@@ -20,12 +27,25 @@ export default function InputField({
           {label}
         </label>
       )}
-      <input
-        id={id}
-        type={type}
-        className={`p-3.5 rounded text-sm border border-stroke ${className}`}
-        {...props}
-      />
+
+      <div className="relative">
+        <input
+          id={id}
+          type={inputType}
+          className={`p-3.5 pr-10 rounded text-sm border w-full  ${className}`}
+          {...props}
+        />
+        {isPassword && hasToggle && (
+          <button
+            type="button"
+            onClick={() => setShow(prev => !prev)}
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+          >
+            {show ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+        )}
+      </div>
+
       {errorMessage && (
         <p className="mt-1 text-xs p-2 text-error">{errorMessage}</p>
       )}
