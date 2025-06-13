9f83e7ef90f701078acd68383d77e156ecb10070
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-14 01:12:36 +0900

    ✨ [new features] MonthlyExpertList 컴포넌트 기능 구현

--- src/components/pages/Expert/MonthlyExpertList.tsx
+++ src/components/pages/Expert/MonthlyExpertList.tsx
@@ -1,13 +1,24 @@
-import type { JSX } from 'react';
+import { useEffect, type JSX } from 'react';
 import Button from '../../common/Button';
+import ExpertCard from './ExpertCard';
+import { useExpertStore } from '../../../stores/useExpertStore';
 
 export default function MonthlyExpertList(): JSX.Element {
+  const { experts, fetchExperts } = useExpertStore();
+
+  useEffect(() => {
+    fetchExperts();
+  }, [fetchExperts]);
+
   return (
-    <div>
-      <div className="flex justify-between p-2">
+    <div className="my-2">
+      <div className="flex justify-between p-4">
         <div className="text-h3">이 달의 엑스퍼트</div>
         <Button variant="text">더보기</Button>
       </div>
+      {experts.map(item => (
+        <ExpertCard key={item.id} {...item} />
+      ))}
     </div>
   );
 }
