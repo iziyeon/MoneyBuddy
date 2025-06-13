7d2a660f90c496bc70e2629dd6566045aaf3a710
iziyeon <dlfvkfclfrn@gmail.com>
2025-06-13 20:10:35 +0900

    ➕ [add] 문서화 및 개발 환경 Mock 핸들러 설정
    - Expert 이미지 자산 추가 (public/jpg/experts/)
    - Mock 핸들러 구조 개선 및 advisor 핸들러 추가
    - 향후 검색 페이지 및 Expert API 구조 생성
    - 개발 환경 개선을 위한 기본 설정 완료"

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,14 @@
+import PageWrapper from '../components/layout/PageWrapper';
+import PageHeader from '../components/layout/PageHeader';
+
+export default function SearchPage() {
+  return (
+    <PageWrapper>
+      <PageHeader title="검색" />
+      <div className="px-5">
+        {/* 검색 컴포넌트 추가 예정 */}
+        <div>검색 페이지</div>
+      </div>
+    </PageWrapper>
+  );
+}
