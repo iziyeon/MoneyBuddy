980fd9a1d29f8598a5ecff6249632db8397f01db
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-21 23:44:31 +0900

    ✨ [new features] PromotionCard 컴포넌트 생성

---
+++ src/components/pages/Home/PromotionCard.tsx
@@ -0,0 +1,43 @@
+import type { JSX } from 'react';
+
+interface PromotionCardProps {
+  text: string;
+  page?: string;
+  bgColor?: string;
+  images: string[];
+}
+
+export default function PromotionCard({
+  text,
+  page,
+  bgColor = 'bg-[#D4DFFF]',
+  images,
+}: PromotionCardProps): JSX.Element {
+  return (
+    <div className={`relative h-24 rounded-lg p-3 mx-3 my-8 ${bgColor}`}>
+      <div className="text-h3 leading-[1.3] text-[#6488FF] whitespace-pre-line">
+        {text}
+      </div>
+
+      {images[0] && (
+        <img
+          src={images[0]}
+          alt="icon-1"
+          className="absolute bottom-11 right-[60px] w-[76px] h-auto z-10"
+        />
+      )}
+
+      {images[1] && (
+        <img
+          src={images[1]}
+          alt="icon-2"
+          className="absolute bottom-1 -right-1 w-32 h-auto z-10"
+        />
+      )}
+
+      <div className="absolute bottom-2 right-2 w-[32px] h-[20px] bg-[#000000] opacity-40 text-white text-[10px] flex items-center justify-center rounded-full z-20">
+        {page}
+      </div>
+    </div>
+  );
+}
